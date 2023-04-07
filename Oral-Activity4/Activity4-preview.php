c3f0769143adbac20fda5435039d5f90b0f29a32
Bülent Oral <96498139+bulent017@users.noreply.github.com>
2023-04-07 23:49:06 +0300

    Update Activity4-preview.php

--- Oral-Activity4/Activity4-preview.php
+++ Oral-Activity4/Activity4-preview.php
@@ -1,11 +1,5 @@
 <html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Form Results</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" href="style.css">
-</head>
+
 <body>
     <?php 
         $name = $_POST["name"] == "" ? "Not provided" : $_POST["name"];
